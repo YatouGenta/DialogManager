e8f86764aa36b1537bb6cc6542dfee95543e2320
YatouGenta <yatougenta@gmail.com>
2019-05-13 10:53:52 +0900

    プロジェクト追加

---
+++ DialogManager/Assets/DialogController/Sample/Scripts/SampleDialog.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// サンプル用ダイアログクラス
+/// </summary>
+public class SampleDialog : AnimationDiadlogBase
+{
+    //タイトルText
+    [SerializeField]
+    private Text titleText = null;
+    //内容Text
+    [SerializeField]
+    private Text contentText = null;
+
+    //初期化
+    public override IEnumerator DialogInitialize(DialogData data = null)
+    {
+        //取得したデータを元に内容を書き換え
+        if (data != null)
+        {
+            SampleDialogData dData = (SampleDialogData)data;
+            titleText.text = dData.title;
+            contentText.text = dData.content;
+        }
+        yield return base.DialogInitialize(data);
+    }
+    //終了時
+    public override IEnumerator DialogFinalize(DialogData data = null)
+    {
+        Debug.Log("終了処理開始");
+        yield return base.DialogFinalize(data);
+    }
+
+    /// <summary>
+    /// 閉じるボタン
+    /// </summary>
+    public void OnClickCloseButton()
+    {
+        Debug.Log("ボタン押した");
+        showDialiogWaitFlg = false;
+    }
+}
+/// <summary>
+/// サンプルダイアログ用データクラス
+/// </summary>
+public class SampleDialogData : DialogData
+{
+    public string title;
+    public string content;
+}
---
+++ DialogManager/Assets/DialogController/Sample/Scripts/SampleDialogShow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleDialogShow : MonoBehaviour
+{
+    public void OnClickShowButton()
+    {
+        SampleDialogData data = new SampleDialogData();
+        data.title = "タイトル書換";
+        data.content = "内容書換";
+        StartCoroutine(DialogManager.instance.DialogShow(DialogSelector.DialogType.SampleDialog, data));
+    }
+}
